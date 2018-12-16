b5a6de6bcb799f9a487b2df7b8eb2d3c65e06e24
Robot-Fromage <code@clementberthaud.com>
2018-12-16 23:49:15 +0100

    infix: Test_DraftsmanCore.hpp header comment

--- Draftsman/Modules/Test_DraftsmanCore/Include/Test_DraftsmanCore.hpp
+++ Draftsman/Modules/Test_DraftsmanCore/Include/Test_DraftsmanCore.hpp
@@ -1,3 +1,15 @@
+/***************************************************************************************
+ *
+ *   Test_DraftsmanCore
+ *__________________
+ *
+ * @file Test_DraftsmanCore.hpp
+ * @author Clement BERTHAUD <code@clementberthaud.com>
+ * @date 16/12/2018
+ * @brief Definition of DraftsmanCore Test Data
+ * @licence MIT License - Copyright (c) 2018-2019 Draftsman - Clement BERTHAUD <code@clementberthaud.com>
+ */
+
 #pragma once
 
 #define TEST_DRAFTSMANCORE
