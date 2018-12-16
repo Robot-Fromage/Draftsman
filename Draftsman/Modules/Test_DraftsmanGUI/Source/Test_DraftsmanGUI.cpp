250b4975b378524455f325337f78a75635cbe87a
Robot-Fromage <code@clementberthaud.com>
2018-12-16 19:48:14 +0100

    infeat: allowing to link with Qt

--- Draftsman/Modules/Test_DraftsmanGUI/Source/Test_DraftsmanGUI.cpp
+++ Draftsman/Modules/Test_DraftsmanGUI/Source/Test_DraftsmanGUI.cpp
@@ -2,17 +2,23 @@
 #include "DraftsmanCore/DraftsmanCore.hpp"
 #include "DraftsmanGUI/DraftsmanGUI.hpp"
 
+#include <QApplication>
+#include <QWidget>
+
 #ifdef TEST_DRAFTSMANGUI
 
-int main()
+int main( int argc, char *argv[] )
 {
-    while(true)
-    {
-        ::DraftsmanCore::test();
-        ::DraftsmanGUI::test();
-    }
+    ::DraftsmanCore::test();
+    ::DraftsmanGUI::test();
 
-    return  0;
+    QApplication app( argc, argv );
+    QWidget w;
+    w.show();
+
+    int exit_code = app.exec();
+
+    return  exit_code;
 }
 
 #endif // TEST_DRAFTSMANGUI
