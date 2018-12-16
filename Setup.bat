ac0ba09b970fc614c3ce8a63c0efd9f5e7452d0f
Robot-Fromage <code@clementberthaud.com>
2018-12-16 17:44:07 +0100

    inwip: some cleaning in the repository infrastructure

--- Setup.bat
+++ Setup.bat
@@ -5,6 +5,6 @@ pushd %~dp0
 git submodule init Draftsman/Tools/Submodules/ProjectDependencies/ProjectDependencies
 REM git submodule init Draftsman/Tools/Submodules/ProjectHooks/ProjectHooks
 call Draftsman/Tools/Scripts/Windows/SetupHooks.bat
-copy "Draftsman\Config\DefaultProjectConfig.ini" "ProjectConfig.ini"
+REM copy "Draftsman\Config\DefaultProjectConfig.ini" "ProjectConfig.ini"
 REM call Draftsman/Tools/Scripts/Windows/ProjectDependencies.bat -d
 pause
