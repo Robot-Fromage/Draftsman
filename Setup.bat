b17dc0a85a06db91b416f9f0c283d7c741174dda
Robot-Fromage <code@clementberthaud.com>
2019-02-24 23:29:33 +0100

    infix: review repo arch a little

--- Setup.bat
+++ Setup.bat
@@ -4,7 +4,7 @@ pushd %~dp0
 
 git submodule update --init Draftsman/Tools/Submodules/ProjectDependencies/ProjectDependencies
 call Draftsman/Tools/Scripts/Windows/SetupHooks.bat
-if not exist "VisualStudioProjectConfig.cmake" copy "Draftsman\Config\DefaultConfig.cmake" "VisualStudioProjectConfig.cmake"
-if not exist "MinGWMakefefilesConfig.cmake" copy "Draftsman\Config\DefaultConfig.cmake" "MinGWMakefefilesConfig.cmake"
+if not exist "VisualStudioProjectConfig.cmake" copy "Draftsman\Tools\DefaultConfig.cmake" "VisualStudioProjectConfig.cmake"
+if not exist "MinGWMakefefilesConfig.cmake" copy "Draftsman\Tools\DefaultConfig.cmake" "MinGWMakefefilesConfig.cmake"
 call Draftsman/Tools/Scripts/Windows/ProjectDependencies.bat -d
 pause
