558a1cb9c648e3b0bc3c78cb8f8ebc27f7660f36
Robot-Fromage <code@clementberthaud.com>
2019-02-24 16:49:35 +0100

    infeat: proper multiconfig generation for MinGW

--- Draftsman/Tools/Scripts/Windows/Generate_MinGW_Makefiles.bat
+++ Draftsman/Tools/Scripts/Windows/Generate_MinGW_Makefiles.bat
@@ -15,11 +15,22 @@ IF NOT EXIST Generated_MinGW ( MKDIR Generated_MinGW )
 :: Step in Generated dir
 cd Generated_MinGW
 
+IF NOT EXIST Release ( MKDIR Release )
+IF NOT EXIST Debug ( MKDIR Debug )
+
+cd Release
 :: Clean cmake garbage if there
 IF EXIST CMakeFiles ( rmdir /S /Q CMakeFiles )
 IF EXIST cmake_install.cmake ( del cmake_install.cmake )
 IF EXIST CMakeCache.txt ( del CMakeCache.txt )
-
 :: Rebuild Project
-SET PATH=%PATH%D:\QtMinGW\Tools\mingw730_64\bin;
-cmake -G "MinGW Makefiles" -DDRAFTSMAN_USE_CONFIG:BOOL=ON ../Modules
+cmake -G "MinGW Makefiles" -DDRAFTSMAN_USE_CONFIG:BOOL=ON -DCMAKE_BUILD_TYPE=Release ../../Modules
+
+cd ..
+cd Debug
+:: Clean cmake garbage if there
+IF EXIST CMakeFiles ( rmdir /S /Q CMakeFiles )
+IF EXIST cmake_install.cmake ( del cmake_install.cmake )
+IF EXIST CMakeCache.txt ( del CMakeCache.txt )
+:: Rebuild Project
+cmake -G "MinGW Makefiles" -DDRAFTSMAN_USE_CONFIG:BOOL=ON -DCMAKE_BUILD_TYPE=Debug ../../Modules
