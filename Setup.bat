1b5b10e3171347a2a1dc50753596063593de3149
Robot-Fromage <code@clementberthaud.com>
2019-02-24 15:37:29 +0100

    infeat: modifier project config override architecture slightly

--- Setup.bat
+++ Setup.bat
@@ -5,6 +5,7 @@ pushd %~dp0
 git submodule init Draftsman/Tools/Submodules/ProjectDependencies/ProjectDependencies
 REM git submodule init Draftsman/Tools/Submodules/ProjectHooks/ProjectHooks
 call Draftsman/Tools/Scripts/Windows/SetupHooks.bat
-copy "Draftsman\Config\DefaultProjectConfig.cmake" "ProjectConfigOverrides.cmake"
+copy "Draftsman\Config\DefaultConfig.cmake" "VisualStudioProjectConfig.cmake"
+copy "Draftsman\Config\DefaultConfig.cmake" "MinGWMakefefilesConfig.cmake"
 REM call Draftsman/Tools/Scripts/Windows/ProjectDependencies.bat -d
 pause
